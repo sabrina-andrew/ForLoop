c586d977e72f3a03b49e1431e116e9d7cad30655
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-30 13:34:50 -0500

    Added user input:

---
+++ ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (int i = userInput; i <= 100; i++ )
+            {
+                
+                Console.WriteLine(i);
+                
+            } 
+             Console.ReadKey();
+        }
+    }
+}
